3b12db328f1baede01650b1efb1346daeb4b053b
Liljack118 <kamilzajaczek4@gmail.com>
2023-02-09 00:50:30 +0100

    create custom writer

--- interfaces_http/main.go
+++ interfaces_http/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("https://google.com")
@@ -13,8 +17,18 @@ func main() {
 		log.Fatal(err)
 	}
 
-	data := make([]byte, 99999) // initialize empty byte slice with 99999 available element spaces
-	// read response body to data slice
-	resp.Body.Read(data)
-	fmt.Println(string(data))
+	// data := make([]byte, 99999) // initialize empty byte slice with 99999 available element spaces
+	// // read response body to data slice
+	// resp.Body.Read(data)
+	// fmt.Println(string(data))
+
+	// Alternative
+	io.Copy(os.Stdout, resp.Body)
+}
+
+// create Write method to implement writer interface
+func (lw logWriter) Write(bs []byte) (int, error) {
+	// print byte slice
+	fmt.Println(string(bs))
+	return len(bs), nil
 }
