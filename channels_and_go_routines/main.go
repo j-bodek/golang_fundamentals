1dc553c3b0cf00ec4793a44717dade46db71fde6
Liljack118 <kamilzajaczek4@gmail.com>
2023-02-09 18:32:22 +0100

    channels and go routines fundamentals

--- channels_and_go_routines/main.go
+++ channels_and_go_routines/main.go
@@ -14,20 +14,31 @@ func main() {
 		"https://stackoverflow.com",
 	}
 
+	// create new channel to allow communication between
+	// child and main routines
+	c := make(chan string) // channel that can be used to send string
+
 	// loop through every link and try to open it
 	for _, link := range websites {
-		fmt.Printf("Trying to open: '%s'\n", link)
-		checkLink(link)
+		// create new go routine (checkLink will be executed in new
+		// go routine to prevent main routine when waiting for response)
+		go checkLink(link, c) // pass channel to checkLink
+	}
+
+	for i := 0; i < len(websites); i++ {
+		fmt.Println(<-c)
 	}
 }
 
 // function used to check if link returns error after get request
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 
 	if err != nil {
 		fmt.Printf("Something wrong with '%s' %s\n\n", link, err)
+		c <- "something went wrong"
 	} else {
 		fmt.Printf("'%s' is up\n\n", link)
+		c <- "website is up"
 	}
 }
